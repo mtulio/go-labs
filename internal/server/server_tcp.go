b247392c43955a574de8adadfaf44a556df1dd81
Marco Braga <mrbraga@redhat.com>
2021-12-08 10:32:11 -0300

    chore/comment: typo on Controller description

--- internal/server/server_tcp.go
+++ internal/server/server_tcp.go
@@ -164,7 +164,6 @@ func (srv *ServerTCP) StartController() {
 			return
 		}
 
-		log.Println(srv.config.hc.GetHealthy(), srv.ServerPortIsOpen())
 		// State> health check is failing and server is up.
 		// Action: Server needs to be stopped
 		if !(srv.config.hc.GetHealthy()) && (srv.ServerPortIsOpen()) {
